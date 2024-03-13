f96f873a0e4cc7a85e6b5aecca342c5a2cd32f2a
Tushar <kmrtushar01@gmail.com>
2024-03-13 20:51:14 +0530

    adding tailwind css in jsx file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import "./App.css";
 
 function App() {
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+    // Wrapper class
+
+    <div className="w-[100vw] h-[100vh] bg-[#344151] flex flex-col justify-center items-center gap-10">
+
+       <div className="text-[#0398d4] font-medium text-2xl">Increment&Decrement</div>
+
+       <div className="flex bg-white justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+
+        <button className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">-</button>
+
+        <div className="font-bold gap-12 text-5xl"></div>
+
+        <button className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">+</button>
+
+       </div>
+
+        <button className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">Reset</button>
+
+    </div>
   );
 }
 
